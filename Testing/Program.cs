1df8ecbe46aa37bf362f8cbbb9588e74b67c6923
badtoro2 <josiah.bull7@gmail.com>
2019-10-07 17:26:57 +1300

    C# Inital Commit

---
+++ Testing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Testing { 
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public void toggleMusic()
+        {
+
+        }
+    }
+}
