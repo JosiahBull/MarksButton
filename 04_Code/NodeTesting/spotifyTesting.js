28a552a633bb69770c293d1dd3f5ff5aa7d81069
badtoro2 <josiah.bull7@gmail.com>
2019-10-29 11:27:22 +1300

    Project Closeout
    - Excess Testing Files Removed
    - Report Written & Saved as PDF

--- 04_Code/NodeTesting/spotifyTesting.js
+++
@@ -1,8 +0,0 @@
-app.get('/login', function(req, res) {
-    var scopes = 'user-read-private user-read-email';
-    res.redirect('https://accounts.spotify.com/authorize' +
-      '?response_type=code' +
-      '&client_id=' + c0f97f78e95442fe8e9d5de49bd99a04 +
-      (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-      '&redirect_uri=' + encodeURIComponent(redirect_uri));
-    });
